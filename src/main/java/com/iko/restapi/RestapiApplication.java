3ea3dfb75741b654860f1fb64317c67f3fe25be2
Jaden Choi <jkya02@gmail.com>
2022-10-14 09:40:12 +0900

    hotfix: add allowedOrigin localhost:3000

--- src/main/java/com/iko/restapi/RestapiApplication.java
+++ src/main/java/com/iko/restapi/RestapiApplication.java
@@ -2,7 +2,10 @@ package com.iko.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing // 시간 자동 기록
 @SpringBootApplication
@@ -12,4 +15,13 @@ public class RestapiApplication {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
