0c378d5864e973965df922baa2a3f6a91329648a
jspgmp <mmriann@gmail.com>
2022-10-27 16:49:22 +0900

    MOD: refreshToken
    1. /auth/refresh로 경로 수정
    2. Authorization 헤더로 토큰 받기
    토큰담는 형식 주의

--- src/main/java/com/iko/restapi/controller/user/TokenController.java
+++ src/main/java/com/iko/restapi/controller/user/TokenController.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,14 +21,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
+@RequestMapping(value="/auth")
 public class TokenController {
 	private final JwtTokenProvider jwtTokenProvider;
 
-	@RequestMapping(value="/refreshToken", method=RequestMethod.POST)
-	public CommonResponse<Map<String, String>> refreshToken(@RequestBody Map<String, String> refreshToken){
-		// TODO: 
+	@RequestMapping(value="/refresh", method=RequestMethod.POST)
+	public CommonResponse<Map<String, String>> refreshToken(@RequestHeader(value="Authorization") String refreshToken){
+		// 토큰담는 형식 주의: RequestHeader.Authorization: Bearer $token
 		try {
-			String token = refreshToken.get("refreshToken");
+			String token = refreshToken.substring(7);
 			Authentication	authentication = jwtTokenProvider.getAuthentication(token, "refreshKey");
 			var principalDetails = (PrincipalDetails) authentication.getPrincipal();
 			String renewedAccessToken = jwtTokenProvider.createAccToken(principalDetails.getUsername(), principalDetails.getAuthorities());
